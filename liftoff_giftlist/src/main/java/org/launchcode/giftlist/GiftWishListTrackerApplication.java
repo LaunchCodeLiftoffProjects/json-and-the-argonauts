bc7400f620820533512b19f44f9b5917710145f0
Mark Thorne <thorne.mark@gmail.com>
2021-12-17 21:14:25 -0600

    Initial commit

---
+++ liftoff_giftlist/src/main/java/org/launchcode/giftlist/GiftWishListTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.giftlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftWishListTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftWishListTrackerApplication.class, args);
+	}
+
+}
---
+++ liftoff_giftlist/src/test/java/org/launchcode/giftlist/GiftWishListTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.giftlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftWishListTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
