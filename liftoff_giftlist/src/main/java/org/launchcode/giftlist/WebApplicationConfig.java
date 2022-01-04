f852ee556ca9f5b113aaf0fbded5116cf7f352af
caparcom <caleb_combs@yahoo.com>
2022-01-03 23:00:06 -0600

    Completed Authentication and passHashing. Ready for merge with Main

--- liftoff_giftlist/src/main/java/org/launchcode/giftlist/WebApplicationConfig.java
+++ liftoff_giftlist/src/main/java/org/launchcode/giftlist/WebApplicationConfig.java
@@ -1,4 +1,25 @@
 package org.launchcode.giftlist;
 
-public class WebApplicationConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+    // Create spring-managed object to allow the app to access our filter
+    @Bean
+    public AuthenticationFilter authenticationFilter() {
+        return new AuthenticationFilter();
+    }
+
+    // Register the filter with the Spring container
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationFilter());
+    }
+
+
+
 }
