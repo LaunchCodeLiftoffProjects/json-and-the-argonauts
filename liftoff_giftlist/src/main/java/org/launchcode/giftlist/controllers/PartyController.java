0a569a38c2e353a7162df6a5ac66bb1e9f439e67
caparcom <caleb_combs@yahoo.com>
2022-01-16 13:57:12 -0600

    added functionality in party controller to show only the groups the current user is a part of

--- liftoff_giftlist/src/main/java/org/launchcode/giftlist/controllers/PartyController.java
+++ liftoff_giftlist/src/main/java/org/launchcode/giftlist/controllers/PartyController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PartyController {
@@ -46,6 +48,30 @@ public class PartyController {
         partyRepository.save(party);
         return "party_list";
     }
+
+    //find how to dynamically change URL based on specific group the user has clicked ${specific group id}
+    @GetMapping("/party_list_add_member")
+    public String renderAddToGroupForm(Model model, HttpSession session, Party party){
+        Integer currentUserId = (Integer) session.getAttribute("user");
+        userRepository.findById(currentUserId);
+        partyRepository.findAllById(Collections.singleton(currentUserId));
+        User user = userRepository.findById(currentUserId).get();
+        List<User> members = party.getMembers();
+        List<Party> parties =  partyRepository.findAllByPartyOwner(user);
+        model.addAttribute("members", members);
+        model.addAttribute("party", partyRepository.findById(currentUserId));
+        return "party_list_add_member";
+    }
+
+    @PostMapping("/party_list_add_member")
+    public String processAddToGroupForm(Model model, @ModelAttribute Party party, String username){
+        User userToAdd = userRepository.findByUsername(username);
+        party.addMember(userToAdd);
+        return "party_list";
+    }
+
+
+
 
     /*@GetMapping("/party_list")
     public String showGroups(Model model, HttpSession session){
--- liftoff_giftlist/src/main/java/org/launchcode/giftlist/repositories/PartyRepository.java
+++ liftoff_giftlist/src/main/java/org/launchcode/giftlist/repositories/PartyRepository.java
@@ -1,10 +1,14 @@
 package org.launchcode.giftlist.repositories;
 
 import org.launchcode.giftlist.models.Party;
+import org.launchcode.giftlist.models.User;
+import org.launchcode.giftlist.models.WishList;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PartyRepository extends CrudRepository<Party, Integer> {
-
+    List<Party> findAllByPartyOwner(User user);
 }
