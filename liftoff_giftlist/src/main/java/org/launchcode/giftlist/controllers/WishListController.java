3170bbd39334dccdd52e400774711f958b5f37bd
s-ewing <samuel.ewing@outlook.com>
2022-01-18 17:49:29 -0600

    Added table to display all lists on wishlists page as well as ability to edit the name and description of each list in the table.

--- liftoff_giftlist/src/main/java/org/launchcode/giftlist/controllers/WishListController.java
+++ liftoff_giftlist/src/main/java/org/launchcode/giftlist/controllers/WishListController.java
@@ -2,6 +2,7 @@ package org.launchcode.giftlist.controllers;
 
 import org.launchcode.giftlist.models.User;
 import org.launchcode.giftlist.models.WishList;
+import org.launchcode.giftlist.models.dto.UpdateWishListDetailsDTO;
 import org.launchcode.giftlist.repositories.UserRepository;
 import org.launchcode.giftlist.repositories.WishListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,13 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -43,4 +46,31 @@ public class WishListController {
         wishListRepository.save(wishList);
         return "user";
     }
+
+    @GetMapping("wishlists")
+    public String displayWishLists(Model model, HttpSession session){
+        Integer currentUserId = (Integer) session.getAttribute("user");
+        User user = userRepository.findById(currentUserId).get();
+        List<WishList> wishlists =  wishListRepository.findAllBylistOwner(user);
+        model.addAttribute("wishlists", wishlists);
+        return "wishlists";
+    }
+
+    @GetMapping("wishlists/{id}")
+    public String displayEditWishListForm(Model model, @PathVariable String id) {
+        WishList wishList = wishListRepository.findById(Integer.parseInt(id)).get();
+        model.addAttribute("wishList", wishList);
+        return "list_details";
+    }
+
+    @PostMapping("wishlists/{id}")
+    public String processEditWishListForm(@PathVariable String id, @Valid @ModelAttribute UpdateWishListDetailsDTO updateWishListDetailsDTO) {
+        WishList wishList = wishListRepository.findById(Integer.parseInt(id)).get();
+        wishList.setName(updateWishListDetailsDTO.getName());
+        wishList.setDescription(updateWishListDetailsDTO.getDescription());
+        wishListRepository.save(wishList);
+        return "redirect:";
+    }
+
+
 }
