6d37cc7be40fdb604703a453bcc73ca332e01704
s-ewing <samuel.ewing@outlook.com>
2022-01-20 18:12:06 -0600

    Added ability to view all items of a particular list, ability to edit item information, and ability to delete items from a list.

--- liftoff_giftlist/src/main/java/org/launchcode/giftlist/controllers/ItemController.java
+++ liftoff_giftlist/src/main/java/org/launchcode/giftlist/controllers/ItemController.java
@@ -3,6 +3,7 @@ package org.launchcode.giftlist.controllers;
 import org.launchcode.giftlist.models.Item;
 import org.launchcode.giftlist.models.User;
 import org.launchcode.giftlist.models.WishList;
+import org.launchcode.giftlist.models.dto.UpdateItemDetailsDTO;
 import org.launchcode.giftlist.repositories.ItemRepository;
 import org.launchcode.giftlist.repositories.UserRepository;
 import org.launchcode.giftlist.repositories.WishListRepository;
@@ -12,8 +13,11 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.List;
@@ -41,11 +45,34 @@ public class ItemController {
 
     @PostMapping("/additem")
     public String processAddItemForm(@ModelAttribute @Valid Item item, Errors errors,
-                                        Model model) {
+                                        Model model, HttpSession session) {
         if (errors.hasErrors()) {
             return "additem";
         }
         itemRepository.save(item);
-        return "user";
+        Integer currentUserId = (Integer) session.getAttribute("user");
+        User user = userRepository.findById(currentUserId).get();
+        List<WishList> wishlists =  wishListRepository.findAllBylistOwner(user);
+        model.addAttribute("wishlists", wishlists);
+        return "redirect:wishlists";
     }
+
+    @GetMapping("wishlists/{listid}/items/edit/{itemid}")
+    public String displayEditItemForm(Model model, @PathVariable String listid, @PathVariable String itemid) {
+        Item item = itemRepository.findById(Integer.parseInt(itemid)).get();
+        model.addAttribute("item", item);
+        WishList wishList = wishListRepository.findById(Integer.parseInt(listid)).get();
+        model.addAttribute("wishList", wishList);
+        return "item_details";
+    }
+
+    @PostMapping("wishlists/{listid}/items/edit/{itemid}")
+    public RedirectView processEditItemForm(@PathVariable String listid, @PathVariable String itemid, @Valid @ModelAttribute UpdateItemDetailsDTO updateItemDetailsDTO) {
+        Item item = itemRepository.findById(Integer.parseInt(itemid)).get();
+        item.setName(updateItemDetailsDTO.getName());
+        item.setDescription(updateItemDetailsDTO.getDescription());
+        itemRepository.save(item);
+        return new RedirectView("/wishlists/" + listid + "/items");
+    }
+
 }
